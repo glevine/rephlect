11e5bb3d94844a9789d4ca99bb0de7209c240285
Greg Levine <levine.greg@gmail.com>
2015-04-30 15:07:58 -0400

    a slim hook is a better way to add routes dynamically

--- index.php
+++ index.php
@@ -3,4 +3,9 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 $rephlect = new \Rephlect\Rephlect($app);
+
+$app->hook('slim.before.router', function() use ($rephlect) {
+    // $rephlect->addResource('class_name');
+});
+
 $app->run();
