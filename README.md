ca133f56d5b0f6daa3c0298f13890f7bbac8d9b3
Greg Levine <levine.greg@gmail.com>
2015-05-04 10:53:02 -0400

    Rephlect is now just middleware for the Slim framework

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 rephlect
 ========
+
+See [rephlect-example](https://github.com/glevine/rephlect-example) for an example of how to use this Slim middleware.
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-$app = new \Slim\Slim();
-$app->add(new \Slim\Middleware\ContentTypes());
-$app->add(new \Rephlect\Rephlect(array('fq_class_name')));
-$app->run();
